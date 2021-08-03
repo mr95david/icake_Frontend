60c7a7ea522cb606d991ed8a065ad14c4d1db290
Miguel Ariza <miguel.ariza@gssanalytix.com>
2021-08-02 23:31:46 -0500

    Cambios en footer, navbar, carousel y secciones

--- src/app/components/jumbo/jumbo.component.ts
+++ src/app/components/jumbo/jumbo.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./jumbo.component.css']
 })
 export class JumboComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit(): void {
