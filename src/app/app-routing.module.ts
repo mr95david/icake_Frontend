079b026bb88ae4a08f15edf3df562cc52adf43f7
Miguel Ariza <miguel.ariza@gssanalytix.com>
2021-08-05 22:14:13 -0500

    adicion de modulo(carpeta)  admin y página de login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes, ExtraOptions} from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FormsContactComponent } from './components/forms-contact/forms-contact.component';
+import { LoginComponent } from './admin/login/login.component';
+
 const routes: Routes = [
   { path: 'header', component: HeaderComponent },
   { path: 'formulario', component: FormsContactComponent },
+  { path: 'login', component: LoginComponent }
 ]
 
 const routerOptions: ExtraOptions = {
